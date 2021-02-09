a4c9d00a3c6fe8c36085df4fd8f80f2abfd94b1f
Aravind <aravind08222@gmail.com>
2021-02-09 20:59:01 +0530

    completed auth

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -110,7 +110,8 @@ class ContactData extends Component{
         const order = { 
             ingredients: this.props.ingredients,
             price: this.props.totalPrice,
-            customer: formData
+            customer: formData,
+            userId: this.props.userId
         }
 
         this.props.onOrderBurger(order, this.props.token);
@@ -209,7 +210,8 @@ const mapStateToProps = state => {
         ingredients: state.burgerBuilder.ingredients,
         totalPrice: state.burgerBuilder.totalPrice,
         loading: state.order.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId
     }
 };
 
